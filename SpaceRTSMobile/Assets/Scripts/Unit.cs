d024d2b88fc3abc4212bb6aa43110e0b1c7dd434
nedio95 <nedio95@gmail.com>
2016-03-21 18:03:13 +0000

    Added files via upload

---
+++ SpaceRTSMobile/Assets/Scripts/Unit.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Unit : MonoBehaviour 
+{
+
+	struct Weapon // Weapons placeholder
+	{
+		enum WEAPON_TYPE
+		{
+			SLUG,
+			LASER,
+			PLASMA
+		}
+	};
+
+	struct Player // Players placeholder
+	{
+
+	};
+
+	List<Weapon> weaponsList = new List<Weapon>(); // List of the weapons currently on the unit
+	int weaponsMaxNumber; // Max number of weapons a unit can hold
+
+	float shieldsMax; // The units maximum shields power
+	float shieldsCurrent; // The current shield power the unit has
+	float shieldRegenPerSec; // The amount of shields the unit regains each second
+
+	float healthMax; // The units maximum health
+	float healthCurrent;// The current amount of health the unit has
+
+	Player ownerOfThis; // The player who owns the unit
+
+	void Start()
+	{
+		healthCurrent = healthMax;
+		shieldsCurrent = shieldsMax;
+
+	}
+}
